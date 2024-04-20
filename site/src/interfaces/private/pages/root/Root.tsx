b1e22135bcbb253981ad1cb400bf96c4c7b235bb
GabriielLeal <joaogabrieel79@gmail.com>
2024-04-19 22:25:18 -0300

    Fazendo a animação da navbar

--- site/src/interfaces/private/pages/root/Root.tsx
+++ site/src/interfaces/private/pages/root/Root.tsx
@@ -16,16 +16,46 @@ import perfil from '/svg/PERFIL.svg';
 import { HiMenu } from "react-icons/hi";
 
 const Root = () => {
+
+    const toggle = () => {
+        //Capturando o elemento sidebar da DOM
+        const sidebar = document.querySelector('.sidebar');
+
+        if (!sidebar) return;
+        //Adicionando e removendo classes para mostrar e esconder o sidebar
+        sidebar.classList.remove('hidden');
+        sidebar.classList.remove('sidebar-hidden');
+        sidebar.classList.add('sidebar-visible');
+
+        //Criando div fantasma para fechar o sidebar
+        const div = document.createElement('div');
+        div.classList.add('overlay');
+        div.style.position = 'fixed';
+        div.style.inset = '0px';
+        div.style.zIndex = '1';
+        div.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+        document.body.appendChild(div);
+
+        div.addEventListener('click', () =>{
+            sidebar.classList.remove('sidebar-visible');
+            sidebar.classList.add('sidebar-hidden');
+            div.remove();
+            setTimeout(() => {
+                sidebar.classList.add('hidden');
+            }, 300);
+        });
+    }
+
     return (
         <div className="h-screen bg-azul-escuro">
             <header>
                 <nav className="w-full h-14 lg:h-12 bg-aurora">
                     <div className="flex px-4 pt-3 lg:pt-2 justify-between items-center">
                         <img className="w-8" src={icone} alt="icone da logo" />
-                        <button className="outline-none text-xl text-azul-claro lg:hidden" aria-label="menu"><HiMenu /></button>
+                        <button className="toggle outline-none text-xl text-azul-claro lg:hidden" aria-label="menu" onClick={()=>{toggle()}}><HiMenu /></button>
                     </div>
                     <div className="lg:flex lg:justify-center lg:-mt-11">
-                        <ul className="private-sidebar p-4 w-48 lg:w-auto h-full fixed lg:sticky top-0 right-0 bg-aurora lg:bg-transparent lg:flex lg:gap-3">
+                        <ul className="sidebar hidden p-4 lg:w-auto h-full fixed lg:sticky top-0 right-0 bg-aurora lg:bg-transparent lg:flex lg:gap-3">
                             <li className="pb-2.5 font-bold uppercase">
                                 <Link className="flex gap-1" to="/painel/dashboard" title="Dashboard" aria-label="Dashboard">
                                     <img src={dashboard} alt="dashboard icone" />Dashboard
